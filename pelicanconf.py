23f7d69a6d9ca6b85f988058db99b50c8996a611
Suzannah Cooper <suzannah.cooper@outlook.com>
2024-08-29 21:27:54 +1000

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'Suzannah Cooper'
+SITENAME = 'Suzannah Cooper'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Australia/Brisbane'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+)
+
+# Social widget
+#SOCIAL = (
+#    ("LinkedIn", "#"),
+#    ("Another social link", "#"),
+#)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
